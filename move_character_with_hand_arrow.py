817fc196c72b3dce7b65ee3ac69fd50ddda3093a
Chaenahyun <nahyun0105@naver.com>
2023-10-01 13:13:41 +0900

    Resolve errors that do not output arrows

--- move_character_with_hand_arrow.py
+++ move_character_with_hand_arrow.py
@@ -2,9 +2,11 @@ from pico2d import *
 import time
 import math
 
+# 화면 크기 설정
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
+# 이미지 로드
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand = load_image('hand_arrow.png')
@@ -23,23 +25,23 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             move_arrow_x, move_arrow_y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT:
+            # 화살표 리스트에 클릭한 위치 추가
             arrow_list.append((event.x, TUK_HEIGHT - 1 - event.y))
 
 def get_distance(x1, y1, x2, y2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
+# 초기화
 running = True
 character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
-#hide_cursor()
 
+# 커서 이미지 로드 및 초기화
+cursor_image = load_image('hand_arrow.png')
 hand_x, hand_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 arrow_list = []
 move_arrow_x, move_arrow_y = hand_x, hand_y
-
 prev_time = time.time()
-
-# move_x와 move_y 변수를 루프 밖에서 정의
 move_x, move_y = 0, 0
 
 while running:
@@ -49,7 +51,8 @@ while running:
     for arrow_x, arrow_y in arrow_list:
         hand.draw(arrow_x, arrow_y)
 
-    # 화살표의 위치를 마우스 이동에 따라 업데이트
+    # 커서 이미지를 마우스 위치 그리기
+    cursor_image.draw(move_arrow_x, move_arrow_y)
     hand_x, hand_y = move_arrow_x, move_arrow_y
 
     if arrow_list:
